c4bcdb3ab84aad2c915fab4edb058a3c0ea5ac9f
vidalfer <80919616+vidalfer@users.noreply.github.com>
2022-09-26 14:41:33 -0300

    Update script_test.py

--- script_test.py
+++ script_test.py
@@ -79,7 +79,7 @@ ret,frame = cap.read()
 frame = cv2.resize(frame,(224,224))
 frame = execute(frame)
 if ret == True:
-cv2.imshow("Frame",frame)
+    cv2.imshow("Frame",frame)
 
     if cv2.waitKey(25) & 0xFF == ord(q):
         break
